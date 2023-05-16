2cfba4c96408422cb95832f78291de601fb26923
cma0022 <cma0022@alu.medac.es>
2023-05-16 16:38:36 +0200

    Seguimos palante

---
+++ PracticaFinalEntornosDesarrollo/src/Reparacion/GestionarReparaciones.java
@@ -0,0 +1,110 @@
+package Reparacion;
+
+import java.util.ArrayList;
+
+import Cliente.Cliente;
+import Vehiculo.Vehiculo;
+
+public class GestionarReparaciones {
+
+	ArrayList<Reparacion> reparaciones = new ArrayList<Reparacion>();
+	
+	/**
+	 * EL MÉTODO SIRVE PARA INSERTAR UNA NUEVA REPARACIÓN EN EL ARRAYLIST
+	 * @param c1 OBJETO CLIENTE QUE MANDA A REPARAR
+	 * @param v1 OBJETO VEHICULO A REPARAR
+	 * @param descripcion STRING DESCRIPCION DE LA REPARACIÓN
+	 * @param fecha STRING FECHA EN LA QUE SE ENTREGA EL COCHE
+	 * @param tiempo STRING TIEMPO QUE VA A DURAR LA REPARACIÓN
+	 * @param totalReparacion DOUBLE PRECIO QUE VA A COSTAR LA REPARACIÓN
+	 * @return ARRAYLIST DE TIPO REPARACION QUE GUARDA LA REPARACION QUE HEMOS INSERTADO
+	 */
+	public void insertarReparacion(Cliente c1, Vehiculo v1, String descripcion, String fecha, String tiempo, double totalReparacion){
+		Reparacion r1 = new Reparacion(c1, v1, descripcion, fecha, tiempo, totalReparacion);
+		this.reparaciones.add(r1);
+		System.out.println("Se ha insertado una nueva reparación.");
+	}
+	
+	/**
+	 * EL MÉTODO SIRVE PARA MODIFICAR UNA REPARACIÓN QUE ME PASAN
+	 * @param r1
+	 * @param c1
+	 * @param v1
+	 * @param descripcion
+	 * @param fecha
+	 * @param tiempo
+	 * @param totalReparacion
+	 * @return
+	 */
+	public Reparacion modificarReparacion(Reparacion r1, Cliente c1, Vehiculo v1, String descripcion, String fecha, String tiempo, double totalReparacion) {
+		r1.setCliente(c1);
+		r1.setVehiculo(v1);
+		r1.setDescripcion(descripcion);
+		r1.setFecha(fecha);
+		r1.setTiempo(tiempo);
+		r1.setTotalReparacion(totalReparacion);
+		System.out.println("Reparación modificada.");
+		return r1;
+	}
+	
+	/**
+	 * EL MÉTODO SIRVE PARA BUSCAR UNA REPARACIÓN POR CLIENTE
+	 * @param c1 OBJETO CLIENTE 
+	 * @return DEVUELVE UN ARRAYLIST DE TIPO REPARACION CON LAS REPARACIONES QUE CONTENGA EL CLIENTE QUE LE HEMOS PASADO
+	 */
+	public ArrayList<Reparacion> buscarReparacionPorCliente(Cliente c1) {
+		ArrayList<Reparacion> reparacionConCliente = new ArrayList<Reparacion>();
+		try{
+			for (Reparacion reparacion : this.reparaciones) {
+				if(reparacion.getCliente().getDni().equals(c1.getDni())) {
+					reparacionConCliente.add(reparacion);
+				}
+			}
+		}catch(Exception e) {
+			System.out.println("No existe ninguna Reparacion que posea ese cliente.");
+		}
+		return reparacionConCliente;
+	}
+	
+	
+	/**
+	 * EL MÉTODO SIRVE PARA BUSCAR UNA REPARACIÓN POR VEHÍCULO
+	 * @param v1 OBJETO VEHICULO
+	 * @return DEVUELVE UN ARRAYLIST DE TIPO REPARACION CON LAS REPARACIONES QUE CONTENGA EL VEHICULO QUE LE HEMOS PASADO
+	 */
+	public ArrayList<Reparacion> buscarReparacionPorVehiculo(Vehiculo v1) {
+		ArrayList<Reparacion> reparacionConVehiculo = new ArrayList<Reparacion>();
+		try{
+			for (Reparacion reparacion : this.reparaciones) {
+				if(reparacion.getVehiculo().getMatricula().equals(v1.getMatricula())) {
+					reparacionConVehiculo.add(reparacion);
+				}
+			}
+		}catch(Exception e) {
+			System.out.println("No existe ninguna Reparacion que posea ese vehículo.");
+		}
+		return reparacionConVehiculo;
+	}
+	
+	
+	/**
+	 * EL MÉTODO SIRVE PARA BUSCAR UNA REPARACIÓN POR FECHA
+	 * @param fecha STRING
+	 * @return DEVUELVE UN ARRAYLIST DE TIPO REPARACION CON LAS REPARACIONES CON LA FECHA QUE LE HAYAMOS PASADO
+	 */
+	public ArrayList<Reparacion> buscarReparacionPorFecha(String fecha) {
+		ArrayList<Reparacion> reparacionConFecha = new ArrayList<Reparacion>();
+		try{
+			for (Reparacion reparacion : this.reparaciones) {
+				if(reparacion.getFecha().equals(fecha)) {
+					reparacionConFecha.add(reparacion);
+				}
+			}
+		}catch(Exception e) {
+			System.out.println("No existe ninguna Reparacion que posea esa fecha.");
+		}
+		return reparacionConFecha;
+	}
+	
+	
+}
